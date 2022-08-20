fa6d00e5b0373da35d850c023e7db583533da07b
aaggarwal10 <anish.5@hotmail.com>
2022-08-20 16:19:06 -0400

    Completed Create Workout UI

--- FitForm/SelectWorkoutView.swift
+++ FitForm/SelectWorkoutView.swift
@@ -14,8 +14,89 @@ struct WorkoutTemplate: Codable {
 }
 
 struct SelectWorkoutView: View {
+    @State private var panelState : String = "Select"
+    @State private var createPopup : Bool = true
+    
+    // Create Workout States
+    @State private var selectedType = "Bicep Curl"
+    @State private var amountReps = ""
+    @State private var weight = ""
+    @State private var workoutName = ""
+    
+    var workoutTypes = ["Bicep Curl", "Jumping Jacks"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                Text("Select Workout")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                    .frame(alignment: .center)
+                Spacer()
+                Button(action: { createPopup = true}) {
+                    Image(systemName: "plus.circle").resizable().aspectRatio(contentMode: .fit)
+                        
+                }
+                .padding(.trailing)
+                .frame(height: 25.0).popover(isPresented: $createPopup) {
+                    ZStack {
+                        NavigationView {
+                            Form {
+                                Section() {
+                                    HStack {
+                                        Text("Workout Name")
+                                        Spacer()
+                                        TextField("", text: $workoutName).multilineTextAlignment(.trailing)
+
+                                    }
+                                    Picker("Type of Workout", selection: $selectedType) {
+                                        ForEach(workoutTypes, id: \.self) { type in
+                                            Text(type)
+                                        }
+                                    }
+                                    HStack {
+                                        Text("Amount of Reps")
+                                        Spacer()
+                                        TextField("#", text: $amountReps).keyboardType(.numberPad).multilineTextAlignment(.trailing)
+
+                                    }
+                                    if selectedType == "Bicep Curl" {
+                                        HStack {
+                                            Text("Amount of Weight")
+                                            Spacer()
+                                            TextField("lbs", text: $weight).keyboardType(.numberPad).multilineTextAlignment(.trailing)
+
+                                        }
+                                    }
+                                }
+                                
+                                Section() {
+                                    Button("Create", action: createWorkout).disabled(workoutName == "" || amountReps == "" || (weight == "" && selectedType == "Bicep Curl"))
+                                }
+                                
+                            }.navigationBarTitleDisplayMode(.inline)
+                                .toolbar {
+                                    ToolbarItem(placement: .principal) {
+                                        Text("Create Workout")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                            .padding(.top)
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+            .padding(.leading)
+            .frame(maxWidth: .infinity)
+        Spacer()
+        }.padding(.vertical)
+    }
+    
+    func createWorkout() {
+        print("Create Workout")
     }
 }
 
@@ -24,3 +105,4 @@ struct SelectWorkoutView_Previews: PreviewProvider {
         SelectWorkoutView()
     }
 }
+
