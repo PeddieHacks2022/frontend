c34ef9c45954171c881cf41d91451cff91b396cc
aaggarwal10 <anish.5@hotmail.com>
2022-08-20 00:40:58 -0400

    Body Tracking Added

---
+++ FitForm/SkeletonJoint.swift
@@ -0,0 +1,13 @@
+//
+//  SkeletonJoint.swift
+//  FitForm
+//
+//  Created by Anish Aggarwal on 2022-08-19.
+//
+
+import Foundation
+
+struct SkeletonJoint {
+    let name: String
+    var position: SIMD3<Float>
+}
