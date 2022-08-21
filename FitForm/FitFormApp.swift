1def13020806a1f3f676afeb063a00ff765a0eb5
Nithin Muthukumar <kathrikai30201094@gmail.com>
2022-08-21 02:23:55 -0400

    splash screen

--- FitForm/FitFormApp.swift
+++ FitForm/FitFormApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FitFormApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            SplashView()
 //            ZStack{
 //
 //                ARViewContainer().edgesIgnoringSafeArea(.all)
---
+++ FitForm/SplashView.swift
@@ -0,0 +1,37 @@
+//
+//  SplashView.swift
+//  FitForm
+//
+//  Created by Nithin Muthukumar on 2022-08-21.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State var isActive: Bool = false
+
+    var body: some View {
+        VStack {
+            if self.isActive {
+                LoginView()
+            } else {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
